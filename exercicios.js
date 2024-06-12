b20710b15471386d379c1f1840e4a5e89c91059d
João Vitor <vitorjoao3264@gmail.com>
2024-06-12 11:55:13 -0300

    Desenhando um triangulo

---
+++ exercicios.js
@@ -0,0 +1,11 @@
+function desenhandoTriangulho(num) {
+  for(let i = 0;i<num;i++){
+      let linha ="";
+      for(let j= 0;j<=i;j++){
+          linha += "#";
+      }
+      console.log(linha);
+  }
+}
+let triangulo = prompt('Quantas linhas voce deseja?')
+console.log(desenhandoTriangulho(triangulo));
